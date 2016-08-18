a554891620fc05a37f9daf8af74e527702fe1a88
gabriel-76 <gabrielrodrigues_76@hotmail.com>
2016-08-18 09:58:43 -0300

    Criando menu

---
+++ SourceCode/Menu/MenuPrincipal.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void menu(){
+    cout << "=============== Menu de Testes =================" << endl
+         << "1- Run static tests" << endl
+         << "2- Run dinamic tests" << endl;
+         
+    char op;
+    cin >> op;
+    
+    switch(op){
+        case '1':
+            break;
+        case'2':
+            break;
+    }
+    
+}
+
+int main() {
+    
+    menu();
+    
+}
